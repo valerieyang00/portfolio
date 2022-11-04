67b8912d925026ce3e262a1d929116346a961668
valerieyang00 <valerieyang00@gmail.com>
2022-11-04 10:47:33 -0700

    progress

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Contact from '../components/contact'
+import Skills from '../components/skills'
 
 export default function Home() {
   return (
@@ -14,7 +15,7 @@ export default function Home() {
           <link rel="icon" href="/favicon.ico" />
           <style>
             @import url({"https://fonts.googleapis.com/css2?family=Meddon&display=swap"});
-            @import url("https://fonts.googleapis.com/css2?family=Saira+Semi+Condensed&display=swap");
+            @import url({"https://fonts.googleapis.com/css2?family=Saira+Semi+Condensed&display=swap"});
           </style>
 
         </Head>
@@ -29,9 +30,6 @@ export default function Home() {
 
         </main>
         <main className={styles.main} id="about">
-          <h1 className="mb-2 font-mono font-semibold text-4xl text-gray-500">
-            hi, {"I'm"} Valerie</h1>
-          <div className="text-3xl font-semibold text-gray-400">Full-stack software engineer with Finance background</div>
 
           <div className="py-16 bg-transparent">
             <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
@@ -44,10 +42,14 @@ export default function Home() {
                     height={500}
                   />
                   <div className="md:12/12 lg:w-10/12 mt-5">
-                  <Contact />
+                    <h1 className="font-semibold text-2xl text-gray-500 text-center">
+                      Valerie Yang </h1>
+                      <div className="text-xl font-semibold text-gray-400 text-center mb-2"> Full-Stack Software Engineer</div> 
+                    <Contact />
                   </div>
                 </div>
                 <div className="md:7/12 lg:w-6/12">
+                  <div className="text-4xl font-mono font-semibold text-gray-400 mb-10">About Me</div>
                   <p className="mt-4 text-gray-600 text-xl"> My passion as a developer grows everyday as I continue on my life-long journey of learning. I would like to take part and contribute in building something that adds value to broad consumers or users. </p>
                   <p className="mt-6 text-gray-600 text-xl">In my 8 years of professional experience, I enjoyed solving problems and the growing technical aspects to my job as finance was being more and more modernized. I am a hard-working professional who has attention to detail, capability to work with different teams and people, and the mindset to develop scalable processes. I hope to bring my experiences, problem-solving skills, and positive energy to the beginning of my new career.</p>
 
@@ -56,13 +58,18 @@ export default function Home() {
               </div>
             </div>
           </div>
+          <h1 className="mb-2 font-mono font-semibold text-3xl text-gray-400">
+            Skills</h1>
+            <Skills />
+
 
         </main>
 
         <main className={styles.main} id="works">
-          <h1 className={styles.title}>
+          <h1 className="text-6xl font-semibold text-gray-500">
             Works
           </h1>
+          <div className="text-3xl font-semibold text-gray-400">I am learning everyday! <p>Please check out some of my work below</p></div>
 
 
           <div className={styles.grid}>
