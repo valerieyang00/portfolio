d1bdcf2196d69a81669adffa120a7e5d8b1cf8ee
valerieyang00 <valerieyang00@gmail.com>
2022-11-06 10:45:59 -0800

    quick fix

--- pages/index.js
+++ pages/index.js
@@ -47,16 +47,16 @@ export default function Home() {
                     height={500}
                   />
                   <div className="md:12/12 lg:w-10/12 mt-5">
-                    <h1 className="font-semibold text-2xl text-gray-500 text-center">
+                    <h1 className="font-semibold text-2xl mb-2 text-gray-500 text-center">
                       Valerie Yang </h1>
-                      <div className="text-xl font-semibold text-gray-400 text-center mb-2"> Full-Stack Software Engineer</div> 
+
                     <Contact />
                   </div>
                 </div>
                 <div className="md:7/12 lg:w-6/12">
                   <div className="text-4xl font-mono font-semibold text-gray-400 mb-10" id={styles.mobileAbout}>About Me</div>
                   <p className="mt-4 text-gray-600 text-xl"> My passion as a developer grows everyday as I continue on my life-long journey of learning. I would like to take part and contribute in building something that adds value to broad consumers or users. </p>
-                  <p className="mt-6 text-gray-600 text-xl">In my previous experience, I enjoyed solving problems and the growing technical aspects to my job as a finance professional. I am hard-working and have attention to detail, capability to work with different teams and people, and the mindset to develop scalable processes. I hope to bring my experiences, problem-solving skills, and positive energy to the beginning of my new career.</p>
+                  <p className="mt-6 text-gray-600 text-xl">In my previous experience, I enjoyed solving problems and the growing technical aspects to my job as a finance professional. I have attention to detail, capability to work with different teams and people, and the mindset to develop scalable processes. I hope to bring my experiences, problem-solving skills, and positive energy to the beginning of my new career.</p>
 
                   <p className="mt-4 text-gray-600 text-xl"> Besides work, I am a cat mom who loves to do cat things with cats 😸</p>
                 </div>
