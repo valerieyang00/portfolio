731c19ba0829f66ef1e0cd19a8b2a02856a7a35f
valerieyang00 <valerieyang00@gmail.com>
2022-11-27 16:50:00 -0800

    testing videos

--- components/projects.js
+++ components/projects.js
@@ -1,16 +1,25 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 import styles from '../styles/Home.module.css'
+import ReactPlayer from 'react-player';
+import {useState, useEffect} from 'react'
+
 
 export default function Projects() {
+    const [hasWindow, setHasWindow] = useState(false);
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            setHasWindow(true);
+        }
+    }, []);
 
     return (
         <>
-        <div className={styles.workContainer}>
+            <div className={styles.workContainer}>
 
-            <div className="flex justify-center" >
-                <div className="card lg:card-side bg-base-100 shadow-xl mb-10 " id={styles.projects}>
-                    <figure>
+                <div className="flex justify-center" >
+                    <div className="card lg:card-side bg-base-100 shadow-xl mb-10 " id={styles.projects}>
+                        <figure>
                         <Carousel className={styles.carousel} >
                             <div>
                                 <img src="/dream1.png" alt="dream1" />
@@ -33,27 +42,27 @@ export default function Projects() {
 
                             </div>
                         </Carousel>
-                    </figure>
-                    <div className="card-body">
-                        <h2 className="card-title mb-5">DreamPlanner</h2>
-                        <p>Fully-responsive web application developed using Django, React, and PostgreSQL <br></br><br></br>Vacation Planning app with savings estimator tool and dashboards dynamically updated for expense vs budget tracking</p>
-                        <p>LOGIN information to explore Live site: <br></br>Email: test@g.com <br></br>Password: tester</p>
-                        <div className="card-footer">
-                            <p>Tech Used: Django, React, PostgreSQL, TailwindCSS</p>
-                            <p>Group Project</p>
-                        </div>
-                        <div className="card-actions justify-end">
-                            <button className="btn scale-100 hover:scale-110 ease-in duration-100" style={{ backgroundColor: "#e66c64" }}><a href="https://dreamplanner-vy.netlify.app/" target="_blank" rel="noopener noreferrer" style={{ color: "black" }}>Live</a></button>
-                            <button className="btn scale-100 hover:scale-110 ease-in duration-100" style={{ backgroundColor: "#e66c64" }}><a href="https://github.com/valerieyang00/dream-planner-client" target="_blank" rel="noopener noreferrer" style={{ color: "black" }}>Github</a></button>
+                        </figure>
+                        <div className="card-body">
+                            <h2 className="card-title mb-5">DreamPlanner</h2>
+                            <p>Fully-responsive web application developed using Django, React, and PostgreSQL <br></br><br></br>Vacation Planning app with savings estimator tool and dashboards dynamically updated for expense vs budget tracking</p>
+                            <p>LOGIN information to explore Live site: <br></br>Email: test@g.com <br></br>Password: tester</p>
+                            <div className="card-footer">
+                                <p>Tech Used: Django, React, PostgreSQL, TailwindCSS</p>
+                                <p>Group Project</p>
+                            </div>
+                            <div className="card-actions justify-end">
+                                <button className="btn scale-100 hover:scale-110 ease-in duration-100" style={{ backgroundColor: "#e66c64" }}><a href="https://dreamplanner-vy.netlify.app/" target="_blank" rel="noopener noreferrer" style={{ color: "black" }}>Live</a></button>
+                                <button className="btn scale-100 hover:scale-110 ease-in duration-100" style={{ backgroundColor: "#e66c64" }}><a href="https://github.com/valerieyang00/dream-planner-client" target="_blank" rel="noopener noreferrer" style={{ color: "black" }}>Github</a></button>
+                            </div>
                         </div>
                     </div>
+
                 </div>
 
-            </div>
-
-            <div className="flex justify-center" >
-                <div className="card lg:card-side bg-base-100 shadow-xl mb-10" id={styles.projects} >
-                    <figure>
+                <div className="flex justify-center" >
+                    <div className="card lg:card-side bg-base-100 shadow-xl mb-10" id={styles.projects} >
+                        <figure>
                         <Carousel className={styles.carousel2}  >
                             <div>
                                 <img src="/weekend1.png" alt="weekend1" />
@@ -86,29 +95,29 @@ export default function Projects() {
 
                             </div> */}
                         </Carousel>
-                    </figure>
-                    <div className="card-body">
-                        <h2 className="card-title mb-5">ThisWeekend</h2>
-                        <p className="mb-3">Full-stack application developed using Node, Express, PostgreSQL.<br></br><br></br>Created an Activity themed application incorporating a web API where users are able to view/save acvities by category, log progress, write reviews, and post & comment on public feeds.</p>
-                        <p>LOGIN information to explore Live site: <br></br>Email: test@g.com <br></br>Password: tester</p>
-                        <div className="card-footer">
-                            <p>Tech Used: Node, Express, PostgreSQL, Sequelize, EJS templates, BootstrapCSS</p>
-                            <p>Solo Project</p>
-                        </div>
-                        <div className="card-actions justify-end">
-                            <button className="btn scale-100 hover:scale-110 ease-in duration-100" style={{ backgroundColor: "#e66c64" }}><a href="https://thisweekend-valerieyang00.koyeb.app/" target="_blank" rel="noopener noreferrer" style={{ color: "black" }}>Live</a></button>
-                            <button className="btn scale-100 hover:scale-110 ease-in duration-100" style={{ backgroundColor: "#e66c64" }}><a href="https://github.com/valerieyang00/Project-2" target="_blank" rel="noopener noreferrer" style={{ color: "black" }}>Github</a></button>
+                        </figure>
+                        <div className="card-body">
+                            <h2 className="card-title mb-5">ThisWeekend</h2>
+                            <p className="mb-3">Full-stack application developed using Node, Express, PostgreSQL.<br></br><br></br>Created an Activity themed application incorporating a web API where users are able to view/save acvities by category, log progress, write reviews, and post & comment on public feeds.</p>
+                            <p>LOGIN information to explore Live site: <br></br>Email: test@g.com <br></br>Password: tester</p>
+                            <div className="card-footer">
+                                <p>Tech Used: Node, Express, PostgreSQL, Sequelize, EJS templates, BootstrapCSS</p>
+                                <p>Solo Project</p>
+                            </div>
+                            <div className="card-actions justify-end">
+                                <button className="btn scale-100 hover:scale-110 ease-in duration-100" style={{ backgroundColor: "#e66c64" }}><a href="https://thisweekend-valerieyang00.koyeb.app/" target="_blank" rel="noopener noreferrer" style={{ color: "black" }}>Live</a></button>
+                                <button className="btn scale-100 hover:scale-110 ease-in duration-100" style={{ backgroundColor: "#e66c64" }}><a href="https://github.com/valerieyang00/Project-2" target="_blank" rel="noopener noreferrer" style={{ color: "black" }}>Github</a></button>
+                            </div>
                         </div>
                     </div>
+
                 </div>
-
             </div>
-        </div>
-        <div className={styles.workContainer}>
+            <div className={styles.workContainer}>
 
-            <div className="flex justify-center" >
-                <div className="card lg:card-side bg-base-100 shadow-xl mb-10" id={styles.projects} >
-                    <figure >
+                <div className="flex justify-center" >
+                    <div className="card lg:card-side bg-base-100 shadow-xl mb-10" id={styles.projects} >
+                        <figure >
                         <Carousel className={styles.carousel3}   >
                             <div>
                                 <img src="/pet1.png" alt="pet1" />
@@ -131,27 +140,39 @@ export default function Projects() {
 
                             </div>
                         </Carousel>
-                    </figure>
-                    <div className="card-body">
-                        <h2 className="card-title mb-5">PetSpace</h2>
-                        <p>Full-Stack application built using MERN <br></br><br></br>Created a photo-upload and social networking application with instagram-like features: follow/unfollow, like/unlike, comments</p>
-                        <p>LOGIN information to explore Live site: <br></br>Email: test@g.com <br></br>Password: tester</p>
-                        <div className="card-footer">
-                            <p>Tech Used: MongoDB, Express, React, Node, Cloudinary, BootstrapCSS</p>
-                            <p>Group Project</p>
-                        </div>
-                        <div className="card-actions justify-end">
-                            <button className="btn scale-100 hover:scale-110 ease-in duration-100" style={{ backgroundColor: "#e66c64" }}><a href="http://petspace-vy.netlify.app" target="_blank" rel="noopener noreferrer" style={{ color: "black" }}>Live</a></button>
-                            <button className="btn scale-100 hover:scale-110 ease-in duration-100" style={{ backgroundColor: "#e66c64" }}><a href="https://github.com/valerieyang00/petspace-p3-client" target="_blank" rel="noopener noreferrer" style={{ color: "black" }}>Github</a></button>
+                        </figure>
+                        <div className="card-body">
+                            <h2 className="card-title mb-5">PetSpace</h2>
+                            <p>Full-Stack application built using MERN <br></br><br></br>Created a photo-upload and social networking application with instagram-like features: follow/unfollow, like/unlike, comments</p>
+                            <p>LOGIN information to explore Live site: <br></br>Email: test@g.com <br></br>Password: tester</p>
+                            <div className="card-footer">
+                                <p>Tech Used: MongoDB, Express, React, Node, Cloudinary, BootstrapCSS</p>
+                                <p>Group Project</p>
+                            </div>
+                            <div className="card-actions justify-end">
+                                <button className="btn scale-100 hover:scale-110 ease-in duration-100" style={{ backgroundColor: "#e66c64" }}><a href="http://petspace-vy.netlify.app" target="_blank" rel="noopener noreferrer" style={{ color: "black" }}>Live</a></button>
+                                <button className="btn scale-100 hover:scale-110 ease-in duration-100" style={{ backgroundColor: "#e66c64" }}><a href="https://github.com/valerieyang00/petspace-p3-client" target="_blank" rel="noopener noreferrer" style={{ color: "black" }}>Github</a></button>
+                            </div>
                         </div>
                     </div>
-                </div>
 
-            </div>
-            <div className="flex justify-center" >
-                <div className="card lg:card-side bg-base-100 shadow-xl mb-10" id={styles.projects} >
-                    <figure >
+                </div>
+                <div className="flex justify-center" >
+                    <div className="card lg:card-side bg-base-100 shadow-xl mb-10" id={styles.projects} >
+                        <figure >
                         <Carousel className={styles.carousel4}  >
+
+                            <div>
+                                {hasWindow && <ReactPlayer url="https://vimeo.com/775603476/bb84ecda7f"
+                                    volume="1"
+                                    width="100%"
+                                    playing={false}
+                                    alt="game1video"
+
+                                />}
+
+
+                            </div>
                             <div>
                                 <img src="/game1.png" alt="game1" />
 
@@ -169,23 +190,23 @@ export default function Projects() {
                             </div>
 
                         </Carousel>
-                    </figure>
-                    <div className="card-body">
-                        <h2 className="card-title mb-5">KittyGame</h2>
-                        <p>Web game designed and built using HTML Canvas and Javascript <br></br><br></br>First project as a student at General Assembly- Software Engineering Immersive</p>
-                        <div className="card-footer">
-                            <p>Tech Used: HTML - Canvas, CSS, Javascript</p>
-                            <p>Solo Project</p>
-                        </div>
-                        <div className="card-actions justify-end">
-                            <button className="btn scale-100 hover:scale-110 ease-in duration-100" style={{ backgroundColor: "#e66c64" }}><a href="https://valerieyang00.github.io/Project-1/" target="_blank" rel="noopener noreferrer" style={{ color: "black" }}>Live</a></button>
-                            <button className="btn scale-100 hover:scale-110 ease-in duration-100" style={{ backgroundColor: "#e66c64" }}><a href="https://github.com/valerieyang00/Project-1" target="_blank" rel="noopener noreferrer" style={{ color: "black" }}>Github</a></button>
+                        </figure>
+                        <div className="card-body">
+                            <h2 className="card-title mb-5">KittyGame</h2>
+                            <p>Web game designed and built using HTML Canvas and Javascript <br></br><br></br>First project as a student at General Assembly- Software Engineering Immersive</p>
+                            <div className="card-footer">
+                                <p>Tech Used: HTML - Canvas, CSS, Javascript</p>
+                                <p>Solo Project</p>
+                            </div>
+                            <div className="card-actions justify-end">
+                                <button className="btn scale-100 hover:scale-110 ease-in duration-100" style={{ backgroundColor: "#e66c64" }}><a href="https://valerieyang00.github.io/Project-1/" target="_blank" rel="noopener noreferrer" style={{ color: "black" }}>Live</a></button>
+                                <button className="btn scale-100 hover:scale-110 ease-in duration-100" style={{ backgroundColor: "#e66c64" }}><a href="https://github.com/valerieyang00/Project-1" target="_blank" rel="noopener noreferrer" style={{ color: "black" }}>Github</a></button>
+                            </div>
                         </div>
                     </div>
-                </div>
 
+                </div>
             </div>
-        </div>
 
 
         </>
